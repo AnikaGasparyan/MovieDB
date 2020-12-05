b862fb799dc7c5f9c50ab6c6c6c777877e6bced1
unknown <ankagasparyan2@gmail.com>
2020-12-05 23:15:47 +0400

    fixed small bugs

--- src/components/header/search-bar.component.js
+++ src/components/header/search-bar.component.js
@@ -2,18 +2,33 @@ import React , {useContext, useState,useEffect} from 'react';
 import { QueryContext } from '../../conexts/query.context';
 import { useDebounce } from '../../hooks/use-debounce.hook';
 import { Redirect } from "react-router-dom";
+// import qs from 'query-string';
 import './header.component.css';
 
 export const SearchBar = () => {
     const [query,setQuery] = useState('');
+    // const [redirectPath,setredirectPath] = useState(null);
     const search = useContext(QueryContext);
     const debouncedQuery = useDebounce(query, 500);
     const [shouldRedirect, setShouldRedirct] = useState(false);
-    const [error, setError] = useState('')
-
+    const [error, setError] = useState('');
+    // const history = useHistory();
+    // const url = qs.parse(history.location.search);
+        
+    // const searchQuery = {
+    //     query: search.query,
+    //     value: search.value,
+    //     page: search.page,
+    //     ...url
+    // }
+    // const location = {
+    //     pathname: '/search-result',
+    //     search : qs.stringify(searchQuery)
+    // };
+    // setredirectPath(location);
+    
     useEffect(()=>{
         search.setQuery(debouncedQuery);
-
         
     },[debouncedQuery, search]);
 
@@ -35,17 +50,17 @@ export const SearchBar = () => {
             setError('Search query is requiered!');
             setShouldRedirct(false);
         }
-        
     };
     const handleSubmit = (e) => {
         e.preventDefault();
         setError('');
         setShouldRedirct(true);
+        
     };
 
     return(
         <>
-            {shouldRedirect && <Redirect  to={`/search-result/?query=${query}`}/>}
+            {shouldRedirect && <Redirect  to={'/search-result'}/>}
             <form onSubmit={handleSubmit}>
                 <div className='input-dropdown'> 
                     <select className='select' onChange={handleValueChange}>
--- src/pages/search-result.page.js
+++ src/pages/search-result.page.js
@@ -6,6 +6,7 @@ import { useSearchResults } from '../hooks/use-search-results.hook';
 import { LoadingScreen } from "../components/loader/loader.component";
 
 export const SearchResult = () => {
+    
     const query = useContext(QueryContext);
     const [movies, resultsLoading] = useSearchResults(query.query, query.value, query.page);
 
@@ -16,23 +17,27 @@ export const SearchResult = () => {
     const handlePrevPageClick = () => {
         query.page !==1 && query.setPage(query.page -1)
     }
-     
+   
     return (
         <>
             {resultsLoading? (<LoadingScreen />) : (
-                <div className='search-results-container'>
+                <> 
+                
                     <div className='error-msg'>
-                        {(!movies.results|| movies.results.length === 0) && <h1> Oops, couldn't find that, try something else</h1>}
+                        {(!movies.results || movies.results.length === 0) && 
+                        <h1> Oops, couldn't find that, try something else</h1>}
                     </div>
+                    <div className='search-results-container'>
                         {movies.results && movies.results.map((movie)=>{
                             console.log(movies)
                         return <MovieCard movie={movie} key={movie.id}/>}
                     )}
-                    <div className='btn-group'>
+                    {movies.results && <div className='btn-group'>
                         <button className='btn'  onClick={handlePrevPageClick}> Previous Page </button>
                         <button  className='btn' onClick={handleNextPageClick}>Next Page</button>
-                    </div>
+                    </div>}
                 </div>
+                </>
 
             )}
         </>
